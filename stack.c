f6cb5e33fb1ec0d2727d7442f5dea5c25d5be29d
rinku-jaat <rinkujaat319@gmail.com>
2026-02-27 14:31:06 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MS 5
+typedef struct
+{
+    int arr[MS];
+    int top;
+} stack;
+void init(stack *s)
+{
+    s->top = -1;
+}
+void push(stack *s, int x)
+{
+    if (s->top == MS - 1)
+    {
+        printf(" jayada hoshiyaar hai kya!!\n");
+        return;
+    }
+    s->top++;
+    s->arr[s->top] = x;
+}
+void display(stack *s)
+{
+    for (int i = 0; i <= s->top; i++)
+    {
+        printf("%d" , s->arr[i]);
+    }
+    printf("\n");
+}
+void pop(stack *s)
+{
+    if (s->top == -1)
+    {
+        printf("Underflow\n");
+        return;
+    }
+    --s->top;
+}
+int getSize(stack *s)
+{
+    int size = s->top + 1;
+    return size;
+}
+int main()
+{
+    stack s;
+    init(&s);
+    push(&s, 10);
+    display(&s);
+
+    push(&s, 20);
+    display(&s);
+    push(&s, 30);
+    display(&s);
+    push(&s, 40);
+    printf("size----->%d", getSize(&s));
+    display (&s);
+    push(&s, 50);
+    display(&s);
+    push(&s ,60);
+    display(&s);
+    pop(&s);
+    display(&s);
+    pop(&s);
+    display(&s);
+    pop(&s);
+    display(&s);
+    pop(&s);
+    display(&s);
+    pop(&s);
+    display(&s);
+    pop(&s);
+    display(&s);
+}
