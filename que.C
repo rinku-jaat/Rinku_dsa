c905aff490d069d607bb6f7426cd7fac1ea16913
rinku-jaat <rinkujaat319@gmail.com>
2026-03-06 13:43:40 +0530

    Add files via upload

---
+++ que.C
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#define MS 3
+typedef struct queue
+{
+    int arr[MS];
+    int front;
+    int rear;
+} queue;
+void init(queue *q)
+{
+    q->front = -1;
+    q->rear = -1;
+}
+void push(queue *q, int x)
+{
+    if (q->rear == MS - 1)
+    {
+        printf(" Jyada hosiyaari nahi!!\n");
+        return;
+    }
+    if (q->front == -1 && q->rear == -1)
+    {
+        q->front = q->front + 1;
+        q->rear = q->rear + 1;
+        q->arr[q->rear] = x;
+    }
+    else
+    {
+        q->rear = q->rear + 1;
+        q->arr[q->rear] = x;
+    }
+}
+void pop(queue *q)
+{
+    if (q->front == -1)
+    {
+        printf(" Underflow\n");
+        return;
+    }
+    if (q->front == q->rear)
+    {
+        q->front = -1;
+        q->rear = -1;
+    }
+    else
+    {
+        q->front = q->front + 1;
+    }
+}
+void display(queue *q)
+{
+    if(q->front==-1 && q->rear==-1)
+    {
+        return;
+    }
+    
+    for (int i = q->front; i <= q->rear; i++)
+    {
+        printf("%d ", q->arr[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    queue q;
+    init(&q);
+    push(&q, 50);
+    display(&q);
+    push(&q, 60);
+    display(&q);
+    push(&q, 70);
+    display(&q);
+    pop(&q);
+    display(&q);
+    pop(&q);
+    display(&q);
+    pop(&q);
+    display(&q);
+    pop(&q);
+    display(&q);
+    
+}
